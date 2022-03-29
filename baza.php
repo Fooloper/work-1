3703585320b0f996ece0a058bbe1428731bc7aa0
Fooloper <102594238+Fooloper@users.noreply.github.com>
2022-03-29 16:49:34 +0500

    added

---
+++ baza.php
@@ -0,0 +1,64 @@
+<?php
+
+// $array = [];
+// $array = array();
+$city = [
+    'Marg\'ilon',
+    'Qo\'qon',
+    'rishton'
+];
+
+$city_key = [
+   "shaxar1"=> [
+       'Marg\'ilon'=>[
+            'istiqlol'=>[
+                'sardor'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 91 326 0650"
+                ],
+                'Diyorbek'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 91 326 0650"
+                ],
+                'Azizbek'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 90 406 16 54"
+                ],
+                'SaidUmaRXon'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 91 326 0650"
+                ]
+            ],
+
+            'Terak tagi'=>[
+                'sardor'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 91 326 0650"
+                ],
+                'Diyorbek'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 91 326 0650"
+                ],
+                'Azizbek'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 91 326 0650"
+                ],
+                'SaidUmaRXon'=>[
+                    'kochasi'=>"Ali qushchi",
+                    'uy'=>"12/24",
+                    'telefon nomeri'=>"+998 91 326 0650"
+                ]
+            ],
+
+       ]
+    ],
+   "shaxar2"=> 'Qo\'qon',
+   "shaxar3"=> 'rishton'
+];
--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php 
+<?php
 
-echo "Salom Jons";
+
+include "baza.php";
+
+?>
+<h1>SHAXAR 1 istiqlol</h1>
+<table border="1">
+    <tr>
+        <th>Ism</th>
+        <th>Kochasi</th>
+        <th>Uyi</th>
+        <th>Telefon nomer</th>
+    </tr>
+    
+        <?php
+            foreach ($city_key['shaxar1']['Marg\'ilon']['istiqlol'] as $key=>$value) {
+                ?>
+                <tr>
+                    <td><?=$key?></td>
+                    <td><?=$value['kochasi']?></td>
+                    <td><?=$value['uy']?></td>
+                    <td><?=$value['telefon nomeri']?></td>
+                </tr>
+                <?php
+            }
+        ?>
+
+    
+</table>
